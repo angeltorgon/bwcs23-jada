e333d86a8eae89a8702ebe2f8c2f339a77866816
angel-torres <alas4224@gmail.com>
2019-11-20 21:23:54 -0600

    working on getting move endpoint to work

--- jada/src/Components/Map.js
+++ jada/src/Components/Map.js
@@ -1,71 +1,100 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Graph } from "react-d3-graph";
+import { axiosWithAuth } from "../utilities/axiosWithAuth";
 
-const data = {
-    nodes: [
-        {
-            "id": "1",
-            "title": "This is room 1",
-            "desc": "This is a generic room called 1.",
-            "n_to": 3,
-            "e_to": 0,
-            "s_to": 0,
-            "w_to": 2
-          },
-          {
-            "id": "2",
-            "title": "This is room 2",
-            "desc": "This is a generic room called 2.",
-            "n_to": 0,
-            "e_to": 1,
-            "s_to": 0,
-            "w_to": 0
-          },
-          {
-            "id": "3",
-            "title": "This is room 3",
-            "desc": "This is a generic room called 3.",
-            "n_to": 0,
-            "e_to": 0,
-            "s_to": 1,
-            "w_to": 0
-          },
-    ]
-        ,
-    links: [
-        { source: "1", target: "3" }, 
-        { source: "1", target: "2" }, 
-        { source: "2", target: "1" }, 
-        { source: "3", target: "1" }, 
-    ],
-};
 
-const config = {
-    nodeHighlightBehavior: true,
-    // staticGraph: false,
-    node: {
-        color: "lightgreen",
-        size: 120,
-        highlightStrokeColor: "blue",
-    },
-    link: {
-        highlightColor: "lightblue",
-    },
-};
 
 const Map = () => {
+    const [data, setData] = useState();
+    const [currentRoom, setCurrentRoom] = useState("1")
+
+    const config = {
+        nodeHighlightBehavior: true,
+        staticGraph: true,
+        node: {
+            color: "lightgreen",
+            size: 120,
+            highlightStrokeColor: "blue",
+        },
+        link: {
+            highlightColor: "lightblue",
+        },
+    };
+
     useEffect(() => {
-        config.staticGraph = true
+        axiosWithAuth()
+        .get("http://127.0.0.1:8000/api/adv/get_rooms")
+        .then((res) => {
+            const arr = []
+            const data = {nodes: [], links: []}
+            for (let [key, value] of Object.entries(res.data.rooms)) {
+                arr.push(value)
+                data.nodes.push({id: key, x: value.x * 50, y: value.y * 50})
+                // data.links.push({source: key, target: value.n_to})
+                if (value.n_to !== 0) {
+                    data.links.push({source:key ,target: String(value.n_to)})
+                }
+                if (value.s_to !== 0) {
+                    data.links.push({source:key ,target: String(value.s_to)})
+                }
+                if (value.e_to !== 0) {
+                    data.links.push({source:key ,target: String(value.e_to)})
+                }
+                if (value.w_to !== 0) {
+                    data.links.push({source:key ,target: String(value.w_to)})
+                }
+            } 
+            console.log(data, "data")
+            setData(data);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
     }, []);
+   
+    // useEffect(() => {
+    //     axiosWithAuth()
+    //     .post("http://127.0.0.1:8000/api/adv/move", { "direction": "s" })
+    //     .then(res => {
+    //         const title = res.data.title.split(" ")
+    //         const newRoom = title[title.length - 1]
+    //         setCurrentRoom(newRoom)
+    //     })
+    //     .catch(err => {
+    //         console.error(err)
+    //     })
+    // }, [currentRoom])
+
+    const moveRoom = () => {
+        axiosWithAuth()
+        .post("http://127.0.0.1:8000/api/adv/move", { "direction": "s" })
+        .then(res => {
+            const title = res.data.title.split(" ")
+            const newRoom = title[title.length - 1]
+            setCurrentRoom(newRoom)
+
+            
+        })
+        .catch(err => {
+            console.error(err)
+        }) 
+    }
+
+    if(!data) return <h1>Loading rooms...</h1>
 
     return (
         <div className='map-container'>
             <h1> Hello from Map</h1>
+            {
+                data.nodes[0].color = "black"
+            }
             <Graph 
+                style={{border: "1px solid red"}}
                 id="graph-id" 
                 data={data} 
                 config={config}
                  />
+            <button onClick={moveRoom}>North</button>
         </div>
     );
 };
